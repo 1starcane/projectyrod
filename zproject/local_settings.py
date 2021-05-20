c6ff831f58189e6d401cc0beed7cf61b284a1549
1starcane <1starcane@gmail.com>
2021-05-20 16:48:00 +0300

    fix

--- zproject/local_settings.py
+++ zproject/local_settings.py
@@ -20,3 +20,4 @@ DATABASES = {
 
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [STATIC_DIR]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
--- zproject/prod_settings.py
+++ zproject/prod_settings.py
@@ -2,13 +2,12 @@ import os
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
-SECRET_KEY = 'django-isdfszcure-i_vi=443#vh6r0g(g_dz2314kdzl*tfae(bs#48z+&+t1hj*'
+SECRET_KEY = 'django-i421zcure-i_vi=443#asdf1dz2314kdzlszht5e(bs#48z+5&+t1hj*'
 DEBUG = True
-ALLOWED_HOSTS = ['projectyrod.ru']
+ALLOWED_HOSTS = [127.0.0.1]
 
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [STATIC_DIR]
-STATIC_ROOT = os.path.join(BASE_DIR, 'collected_static')
 
 
 DATABASES = {
